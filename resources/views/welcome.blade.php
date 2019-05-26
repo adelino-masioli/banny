d4c84f0051f123ae8e643327ce4a74eddf5596ec
alfjuniorbh <alfjuniorbh.web@gmail.com>
2019-05-26 18:23:13 -0300

    Instascan test plugin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,7 +52,7 @@
     </style>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="{{asset('pwa/instascan.min.js')}}"></script>
+    <script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
 </head>
 
 <body>
