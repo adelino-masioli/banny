08b8d44271f95c2262ecfe39555060e58c35089e
alfjuniorbh <alfjuniorbh.web@gmail.com>
2019-05-26 18:26:46 -0300

    Cam 100%

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,10 +44,7 @@
         }
 
         #preview {
-            display: block;
-            width: 354px;
-            height: 354px;
-            object-fit: fill;
+            width: 100%;
         }
     </style>
 
@@ -56,12 +53,7 @@
 </head>
 
 <body>
-    <section>
-        <div class="content">
-            <h1> BANNY</h1>
-            <video width="354" height="354" id="preview"></video>
-        </div>
-    </section>
+    <video id="preview"></video>
 </body>
 
 
