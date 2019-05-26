611c3fe6efb6e2b9f7abe08652534e51ae9d5786
alfjuniorbh <alfjuniorbh.web@gmail.com>
2019-05-26 18:19:02 -0300

    Modify size cam

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,17 +35,31 @@
             align-content: center;
             justify-content: center;
         }
+
+        h1 {
+            font-weight: 300;
+            margin: 0 0 5px 0;
+            font-size: 14px;
+            text-align: center;
+        }
+
+        #preview {
+            display: block;
+            width: 354px;
+            height: 354px;
+            object-fit: fill;
+        }
     </style>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
+    <script src="{{asset('pwa/instascan.min.js')}}"></script>
 </head>
 
 <body>
     <section>
         <div class="content">
             <h1> BANNY</h1>
-            <video id="preview"></video>
+            <video width="354" height="354" id="preview"></video>
         </div>
     </section>
 </body>
@@ -75,7 +89,7 @@
       });
       Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
-          scanner.start(cameras[1]);
+          scanner.start(cameras[0]);
         } else {
           console.error('No cameras found.');
         }
